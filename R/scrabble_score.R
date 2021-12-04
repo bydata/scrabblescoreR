29c2d82404e3bdc111684a9a57db62eb5429f212
bydata <ansgar.wolsing@gmail.com>
2021-12-05 00:31:50 +0100

    add a flag to ignore invalid characters

--- R/scrabble_score.R
+++ R/scrabble_score.R
@@ -1,6 +1,6 @@
-#' Calculate the SCRABBLE score
+#' Calculate the Scrabble score
 #'
-#' Calculate the SCRABBLE score for a word or a vector of words in a given language.
+#' Calculate the Scrabble score for a word or a vector of words in a given language.
 #'
 #' @param words A character value or a character vector
 #' @param lang The language to base the score on. Currently English (en) and
@@ -8,8 +8,11 @@
 #' @param ignore_whitespace Boolean. Shall whitespace be removed from words before
 #'   calculating the score? If FALSE (the default), any whitespace will result in
 #'   an NA value
+#' @param ignore_invalid_characters Boolean. Shall characters which are not part
+#' of the language's Scrabble alphabet be ignored (i.e. 0 points)?
+#'   If FALSE (the default), any whitespace will result in an NA value
 #'
-#' @return A named vector of the same length as `words` with the SCRABBLE score
+#' @return A named vector of the same length as `words` with the Scrabble score
 #'   for each word
 #' @export
 #'
@@ -18,13 +21,22 @@
 #'   scrabble_score(c("rstats", "package"))
 scrabble_score <- function(words,
                            ignore_whitespace = FALSE,
+                           ignore_invalid_characters = FALSE,
                            lang = c("de", "en")) {
   stopifnot("Language currently not supported." = lang %in% supported_languages())
   if (missing(lang)) lang <- "de"
+  words2 <- words
   if (ignore_whitespace) {
-    words <- gsub("\\s", "", words)
+    words2 <- gsub("\\s", "", words2)
   }
-  l <- strsplit(words, split = "")
+  if (ignore_invalid_characters) {
+    invalid_letters_regex <- paste0(
+      "[^",
+      paste(names(letter_scores[["de"]]), collapse = ""),
+      "]")
+    words2 <- gsub(invalid_letters_regex, "", words2)
+  }
+  l <- strsplit(words2, split = "")
   scores <- sapply(l, score, lang = lang)
   names(scores) <- words
   scores
@@ -35,7 +47,7 @@ scrabble_score <- function(words,
 #' @param x a character vector
 #' @param lang The language to base the score on. Currently only German (de) supported
 #'
-#' @return The SCRABBLE score for the input character vector
+#' @return The Scrabble score for the input character vector
 score <- function(x, lang) {
   x <- toupper(x)
   sum(letter_scores[[lang]][x])
